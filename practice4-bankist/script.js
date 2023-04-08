c4679d04e49c2dd088f2e6bf602d10bae6fd70cc
emretantu <emretantu@gmail.com>
2023-04-08 06:49:07 +0300

    displaying movements implemented

--- practice4-bankist/script.js
+++ practice4-bankist/script.js
@@ -61,7 +61,20 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+  movements.forEach(function(mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__value">${mov} EUR</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+}
+displayMovements(account1.movements);
 
 const createUsernames = function (accounts) {
   accounts.forEach(function(acc) {
